a24e9871655652958f048c34470ad5705684f53a
Edgar Figueroa <efiguerc@gmail.com>
2017-03-17 21:55:39 -0600

    seup users#create controller tests

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -3,17 +3,41 @@ require 'rails_helper'
 RSpec.describe Api::V1::UsersController, type: :controller do
 
   describe "GET #show" do
-    before(:each) do
-      @user = FactoryGirl.create :user
-      get :show, id: @user.id, format: :json
-    end
+    let(:user) { FactoryGirl.create :user }
 
     it "returns the information about a user in hash" do
-      user_response = JSON.parse(response.body, symbolize_names: true)
+      get :show, id: user.id, format: :json
+      user_response = json(response.body)
 
-      expect(user_response[:email]).to eq @user.email
+      expect(user_response[:email]).to eq user.email
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "POST #create" do
+
+    context "when is successfully created" do
+      let(:user_attributes) { FactoryGirl.attributes_for :user }
+
+      it "renders a json representation for the user just created" do
+        post :create, { user: user_attributes }, format: :json
+        user_response = json(response.body)
+
+        expect(user_response[:email]).to eq user_attributes[:email]
+        expect(response).to have_http_status(:success)
+      end
     end
 
-  it { should respond_with 200 }
+    context "when it is NOT creted" do
+      let(:invalid_user_attributes) { {password: "12345678", password_confirmation: "12345678"} }
+
+      it "renders an errors json" do
+        post :create, { user: invalid_user_attributes }, format: :json
+        user_response = json(response.body)
+
+        expect(user_response[:errors][:email]).to include "can't be blank"
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
   end
 end
