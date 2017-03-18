1b411c9d54395fb3c659818209ec71cfbc41ca82
Edgar Figueroa <efiguerc@gmail.com>
2017-03-18 00:46:12 -0600

    add users#create controller action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,4 +4,19 @@ class Api::V1::UsersController < ApplicationController
   def show
     respond_with User.find(params[:id])
   end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user, status: :created, location: [:api_v1, user]
+    else
+      render json: { errors: user.errors }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 end
