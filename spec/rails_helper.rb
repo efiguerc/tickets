443af7dcb6106a9f8f9949e7e487deea00eccdec
Edgar Figueroa <efiguerc@gmail.com>
2017-03-17 21:54:29 -0600

    define json method in spec/rails_helper.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -54,6 +54,10 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  
+  def json(body)
+    JSON.parse(body, symbolize_names: true)
+  end
 end
 
 Shoulda::Matchers.configure do |config|
