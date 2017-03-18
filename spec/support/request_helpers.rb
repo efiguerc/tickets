a740bf422372cd96c3c98fb5f8a4aea45f203ce4
Edgar Figueroa <efiguerc@gmail.com>
2017-03-18 12:38:00 -0600

    setup an improved json_response helper

---
+++ spec/support/request_helpers.rb
@@ -0,0 +1,11 @@
+module Request
+  module  JsonHelpers
+    def json_response
+      @json_response ||= JSON.parse(response.body, symbolize_names: true)
+    end
+  end
+end
+
+RSpec.configure do |config|
+  config.include Request::JsonHelpers, type: :controller
+end
