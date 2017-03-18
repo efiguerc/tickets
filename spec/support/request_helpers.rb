460465ab693bb091a9e882b3cefcba21ef22b18f
Edgar Figueroa <efiguerc@gmail.com>
2017-03-18 13:01:59 -0600

    setup default headers in spec/request_helpers.rb

--- spec/support/request_helpers.rb
+++ spec/support/request_helpers.rb
@@ -4,8 +4,19 @@ module Request
       @json_response ||= JSON.parse(response.body, symbolize_names: true)
     end
   end
+
+  module HeadersHelpers
+    def include_default_accept_headers(format = Mime::JSON)
+      request.headers['Accept']       = "#{request.headers['Accept']},#{format}"
+      request.headers['Content-Type'] = format.to_s
+    end
+  end
 end
 
 RSpec.configure do |config|
   config.include Request::JsonHelpers, type: :controller
+  config.include Request::HeadersHelpers, type: :controller
+  config.before(:each, type: :controller) do
+    include_default_accept_headers
+  end
 end
