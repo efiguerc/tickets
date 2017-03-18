7bec034fb02fdf9a19bb98fe34dd7a22605e1101
Edgar Figueroa <efiguerc@gmail.com>
2017-03-17 20:26:18 -0600

    add user#show api route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
+require 'api_constraints'
+
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # devise_for :users
+
+  # Api definition
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :users, only: [:show]
+    end
+  end
 end
