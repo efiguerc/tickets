776df35ece606aa51c38e6a93c015cd7b6cdcb3d
Edgar Figueroa <efiguerc@gmail.com>
2017-03-19 16:02:51 -0600

    setup #authenticate_with_token! controller concern

--- spec/controllers/concerns/authenticable_spec.rb
+++ spec/controllers/concerns/authenticable_spec.rb
@@ -6,13 +6,14 @@ RSpec.describe Authenticable do
       include Authenticable
       def request
       end
+      def response
+      end
     end
     Authentication.new
   }
+  let(:user) { create(:user) }
 
   describe '#current_user' do
-    let(:user) { create(:user) }
-
     before do
       request.headers["Authorization"] = user.access_token
       allow(authentication).to receive(:request).and_return(request)
@@ -20,6 +21,20 @@ RSpec.describe Authenticable do
 
     it "returns the corresponding user" do
       expect(authentication.current_user.access_token).to eq user.access_token
+    end
+  end
+
+  describe '#authenticate_with_token' do
+    before do
+      allow(authentication).to receive(:current_user).and_return(nil)
+      allow(response).to receive(:response_code).and_return(401)
+      allow(response).to receive(:body).and_return({ errors: "Not authenticated" }.to_json)
+      allow(authentication).to receive(:response).and_return(response)
+    end
+
+    it "renders a json error message" do
+      expect(json_response[:errors]).to  eq "Not authenticated"
+      expect(authentication).to respond_with :unauthorized
     end
   end
 end
