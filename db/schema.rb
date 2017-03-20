089d7e8b65bc0315eaa2da209a8285524f8cdc56
Edgar Figueroa <efiguerc@gmail.com>
2017-03-20 17:55:13 -0600

    add role column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170320203516) do
+ActiveRecord::Schema.define(version: 20170320221504) do
 
   create_table "tickets", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "customer_id"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20170320203516) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "access_token",           default: ""
+    t.integer  "role"
     t.index ["access_token"], name: "index_users_on_access_token", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
