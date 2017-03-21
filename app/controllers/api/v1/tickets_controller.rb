0a7f56ce7a277a337718d4a4dd67fab09b817dba
Edgar Figueroa <efiguerc@gmail.com>
2017-03-21 13:31:05 -0600

    add tickets #create #update #destroy controller actions

--- app/controllers/api/v1/tickets_controller.rb
+++ app/controllers/api/v1/tickets_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::TicketsController < ApplicationController
+  before_action :authenticate_with_token!, only: [:create, :update]
+
   respond_to :json
 
   def show
@@ -7,5 +9,35 @@ class Api::V1::TicketsController < ApplicationController
 
   def index
     respond_with Ticket.all
+  end
+
+  def create
+    ticket = current_user.opened_tickets.build(ticket_params)
+    if ticket.save
+      render json: ticket, status: :created, location: [:api_v1, ticket]
+    else
+      render json: { errors: ticket.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    ticket = current_user.opened_tickets.find(params[:id])
+    if ticket.update(ticket_params)
+      render json: ticket, status: :ok, location: [:api_v1, ticket]
+    else
+      render json: { errors: ticket.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    ticket = current_user.opened_tickets.find(params[:id])
+    ticket.destroy
+    head :no_content
+  end
+
+  private
+
+  def ticket_params
+    params.require(:ticket).permit(:category, :title, :description, :priority, :customer_id)
   end
 end
--- spec/controllers/api/v1/tickets_controller_spec.rb
+++ spec/controllers/api/v1/tickets_controller_spec.rb
@@ -23,4 +23,78 @@ RSpec.describe Api::V1::TicketsController, type: :controller do
       expect(response).to             have_http_status :ok
     end
   end
+
+  describe "POST #create" do
+    let(:user) { create(:user, role: 'customer') }
+    let(:ticket_attributes) { attributes_for(:ticket) }
+
+    before(:each) do
+      api_authorization_header user.access_token
+    end
+
+    context "when is succesfully created" do
+      
+      it "renders the json representation for the ticket" do
+        post :create, params: { customer_id: user.id, ticket: ticket_attributes }
+
+        expect(json_response[:title]).to  eq ticket_attributes[:title]
+        expect(response).to               have_http_status :created
+      end
+    end
+
+    context "when is not created" do
+
+      it "renders an errors json" do
+        ticket_attributes[:title] = ""
+        post :create, params: { customer_id: user.id, ticket: ticket_attributes }
+
+        expect(json_response).to                  have_key :errors
+        expect(json_response[:errors][:title]).to include "can't be blank"
+        expect(response).to                       have_http_status :unprocessable_entity
+      end
+    end
+  end
+
+  describe "PUT/PATCH #update" do
+    let(:ticket) { create(:ticket) }
+    
+    before(:each) do
+      api_authorization_header ticket.customer.access_token
+    end
+
+    context "when is succesfully updated" do
+
+      it "renders the json representation for the updated ticket" do
+        patch :update, params: { id: ticket.id, ticket: { title: "A new title" } }
+
+        expect(json_response[:title]).to  eq "A new title"
+        expect(response).to               have_http_status :ok
+      end
+    end
+
+    context "when is not updated" do
+
+      it "renders an errors json" do
+        patch :update, params: { id: ticket.id, ticket: { title: "" } }
+
+        expect(json_response).to                  have_key :errors
+        expect(json_response[:errors][:title]).to include "can't be blank"
+        expect(response).to                       have_http_status :unprocessable_entity
+      end
+    end
+  end
+
+  describe "DELETE #destroy" do
+    let(:ticket) { create(:ticket) }
+    
+    before(:each) do
+      api_authorization_header ticket.customer.access_token
+    end
+    
+    it "is succesfully destroyed" do
+      delete :destroy, params: { id: ticket.id }
+
+      expect(response).to have_http_status :no_content
+    end
+  end
 end
