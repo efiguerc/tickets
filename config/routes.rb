d857cd10c610680ec9ae05a952a3e0466963dc8e
Edgar Figueroa <efiguerc@gmail.com>
2017-03-20 22:29:32 -0600

    setup tickets#show route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [:show, :create, :update, :destroy]
       resources :sessions, only: [:create, :destroy]
+      resources :tickets, only: [:show]
     end
   end
 end
---
+++ spec/routing/api/v1/tickets_routing_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe 'Tickets API routing specs' do
+
+  it 'show a ticket' do
+    expect(get: "/api/v1/tickets/1").to route_to(
+      format:     :json,
+      controller: "api/v1/tickets",
+      action:     "show",
+      id:         '1')
+  end 
+end
