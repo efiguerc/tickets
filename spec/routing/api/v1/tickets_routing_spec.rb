7efe4fab1bbf319e56c902176d444226e9e35382
Edgar Figueroa <efiguerc@gmail.com>
2017-03-21 13:32:19 -0600

    add api v1 tickets #create #update #destroy routes

--- spec/routing/api/v1/tickets_routing_spec.rb
+++ spec/routing/api/v1/tickets_routing_spec.rb
@@ -16,4 +16,27 @@ describe 'Tickets API routing specs' do
       controller: "api/v1/tickets",
       action:     "index")
   end 
+
+  it 'create a ticket' do
+    expect(post: "/api/v1/tickets").to route_to(
+      format:     :json,
+      controller: "api/v1/tickets",
+      action:     "create")
+  end 
+
+  it 'update a ticket' do
+    expect(patch: "/api/v1/tickets/1").to route_to(
+      format:     :json,
+      controller: "api/v1/tickets",
+      action:     "update",
+      id:         '1')
+  end 
+  it 'update a ticket' do
+
+    expect(delete: "/api/v1/tickets/1").to route_to(
+      format:     :json,
+      controller: "api/v1/tickets",
+      action:     "destroy",
+      id:         '1')
+  end 
 end
