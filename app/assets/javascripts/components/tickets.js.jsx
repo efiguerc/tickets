39c3a432de06162f1024f3f583c0d6909f44ff9e
Edgar Figueroa <efiguerc@gmail.com>
2017-03-22 10:45:24 -0600

    setup Tickets component

---
+++ app/assets/javascripts/components/tickets.js.jsx
@@ -0,0 +1,66 @@
+class Tickets extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      tickets: []
+    };
+  }
+
+  componentDidMount() {
+    var _this = this;
+    $.ajax({
+      url: '/api/v1/tickets',
+      dataType: 'json',
+      method: 'GET',
+      beforeSend(xhr) {
+        xhr.setRequestHeader('Accept', 'application/json');
+        xhr.setRequestHeader('Content-Type', 'application/json');
+      },
+      success(data) {
+        _this.setState({tickets: data});
+      },
+      error(xhr, ststus, err) {
+        e = {name: 'loadallReportNews', message: 'err: "' + err + '" Status: "'  + xhr.status + '" responseText:   ' + xhr.responseText};
+      }
+    });  
+  }
+
+  render () {
+    return (
+      <div>
+        <table className="table table-striped">
+          <thead>
+          <tr>
+            <th>Number</th>
+            <th>Category</th>
+            <th>Title</th>
+            <th>Status</th>
+            <th>Priority</th>
+            <th>Customer</th>
+            <th>Created</th>
+            <th>Updated</th>
+            <th>Agent</th>
+          </tr>
+          </thead>
+          <tbody>
+            { this.state.tickets.map( ticket =>
+              <Ticket
+                key={ ticket["id"] }
+                id={ ticket["id"] }
+                category={ ticket["category"] }
+                title={ ticket["title"] }
+                status={ ticket["status"] }
+                priority={ ticket["priority"] }
+                customer_id={ ticket["customer_id"] }
+                created_at={ ticket["created_at"] }
+                updated_at={ ticket["updated_at"] }
+                agent_id={ ticket["agent_id"] } />
+            )}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
