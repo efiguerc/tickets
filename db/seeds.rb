3a4bab07c1325f0fea05fa38dbc0124f936c6465
Edgar Figueroa <efiguerc@gmail.com>
2017-03-22 10:40:26 -0600

    add tickets seeds to the DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,40 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Ticket.delete_all
+User.delete_all
+
+user = User.create!(
+  email:                  "foo.bar@example.com",
+  password:               "12345678",
+  password_confirmation:  "12345678",
+  role:                   "customer"
+)
+
+Ticket.create!(
+  id:           0,
+  category:     "Network",
+  title:        "Network unreachable.",
+  description:  "No answer from any production server when doing ping.",
+  status:       "opened",
+  priority:     "low",
+  customer_id:  user.id,
+  agent:        nil
+)
+
+10.times do |n| 
+  Ticket.create!(
+    category:     "Network",
+    title:        "#{n} Network unreachable.",
+    description:  "No answer from any production server when doing ping.",
+    status:       case n % 3
+                    when 0 then 'opened'
+                    when 1 then 'assigned'
+                    when 2 then 'closed'
+                  end,
+    priority:     case n % 3
+                    when 0 then 'low'
+                    when 1 then 'medium'
+                    when 2 then 'high'
+                  end,
+    customer_id:  user.id,
+    agent:        nil
+  )
+end
