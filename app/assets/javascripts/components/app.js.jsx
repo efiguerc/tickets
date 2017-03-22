f892aa2892e36029cd1f12a8f96f5a5c625cc5a4
Edgar Figueroa <efiguerc@gmail.com>
2017-03-22 10:46:18 -0600

    setup routing in App component

--- app/assets/javascripts/components/app.js.jsx
+++ app/assets/javascripts/components/app.js.jsx
@@ -1,11 +1,30 @@
 class App extends React.Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      current_route: "home"
+    };
+  }
+
+  _setRoute(route) {
+    this.setState({current_route: route});
+  }
+
   render () {
     return (
       <div>
         <Header />
         <div className="container">
-          <Tickets />
+
+          {(() => {
+            switch (this.state.current_route) {
+              case "home":    return <Home setRoute={ this._setRoute.bind(this) } />
+              case "tickets": return <Tickets setRoute={ this._setRoute.bind(this) } />
+            }
+          })()}
+
           <Footer />
         </div>
       </div>
