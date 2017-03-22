bf659ab5b1dc093248466a0d37ef269e32f3d301
Edgar Figueroa <efiguerc@gmail.com>
2017-03-22 10:45:46 -0600

    setup Ticket component

---
+++ app/assets/javascripts/components/ticket.js.jsx
@@ -0,0 +1,18 @@
+class Ticket extends React.Component {
+
+  render () {
+    return (
+      <tr>
+        <td>{ this.props.id }</td>
+        <td>{ this.props.category }</td>
+        <td>{ this.props.title }</td>
+        <td>{ this.props.status }</td>
+        <td>{ this.props.priority }</td>
+        <td>{ this.props.customer_id }</td>
+        <td>{ this.props.created_at }</td>
+        <td>{ this.props.updated_at}</td>
+        <td>{ this.props.agent_id }</td>
+      </tr>
+    );
+  }
+}
