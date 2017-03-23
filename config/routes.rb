1335379cbba4a8573abb3f63cfda5e72ec7ea6b5
Edgar Figueroa <efiguerc@gmail.com>
2017-03-22 19:21:17 -0600

    add api v1 users#index route and controller action

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # API DEFINITION
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :users, only: [:show, :create, :update, :destroy]
+      resources :users, only: [:index, :show, :create, :update, :destroy]
       resources :sessions, only: [:create, :destroy]
       resources :tickets, only: [:show, :index, :create, :update, :destroy]
     end
