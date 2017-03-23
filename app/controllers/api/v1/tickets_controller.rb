8a4e6a09a1d72f2db1b9ad7c4537b873887a2aed
Edgar Figueroa <efiguerc@gmail.com>
2017-03-23 06:20:37 -0600

    update authorization in tickets controller

--- app/controllers/api/v1/tickets_controller.rb
+++ app/controllers/api/v1/tickets_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::TicketsController < ApplicationController
-  before_action :authenticate_with_token!, only: [:create, :update]
+  before_action :authenticate_with_token!, only: [:create, :update, :destroy]
 
   respond_to :json
 
