88d402ecf79b507d4579bee02767e4a081a8b511
Edgar Figueroa <efiguerc@gmail.com>
2017-03-22 19:22:55 -0600

    setup user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,16 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email
+  attributes  :id,
+              :email,
+              :created_at,
+              :updated_at,
+              :access_token,
+              :role
+
+  def created_at
+    "#{object.created_at.strftime("%m-%d-%Y")}"
+  end
+
+  def updated_at
+    "#{object.updated_at.strftime("%m-%d-%Y")}"
+  end
 end
