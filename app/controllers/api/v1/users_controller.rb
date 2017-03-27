bcfb1a97411e81f00171b4573c54d0e8d81bcdf7
Edgar Figueroa <efiguerc@gmail.com>
2017-03-27 12:50:39 -0600

    Do not authorize any user action for customer or admin

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::UsersController < ApplicationController
   before_action :authenticate_with_token!
+  before_action :authorize_action
 
   respond_to :json
 
@@ -39,6 +40,12 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :role)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :role)
+  end
+
+  def authorize_action
+    if current_user.customer? || current_user.agent?
+      render json: { errors: "Not authorized!" }, status: :unauthorized
+    end
   end
 end
